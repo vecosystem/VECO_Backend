6f24cf559411cdf637acfcb6428cd71b392b0c53
rlawngjs0313 <rlawngjs0313@naver.com>
2025-07-07 17:17:08 +0900

    🚀 chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/Veco/VecoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Veco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Veco/VecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Veco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
