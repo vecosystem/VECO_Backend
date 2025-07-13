6f86504c805ce484ef828096d31b421b4cca4279
rlawngjs0313 <juheon0313@naver.com>
2025-07-13 19:45:44 +0900

    ✨ feat: S3 Config, util 생성 (사진 업로드, URL 조회, 삭제)

---
+++ src/main/java/com/example/Veco/global/aws/exception/S3Exception.java
@@ -0,0 +1,9 @@
+package com.example.Veco.global.aws.exception;
+
+import com.example.Veco.global.aws.exception.code.S3ErrorCode;
+
+public class S3Exception extends RuntimeException {
+  public S3Exception(S3ErrorCode message) {
+    super(message.getMessage());
+  }
+}
---
+++ src/main/java/com/example/Veco/global/aws/exception/code/S3ErrorCode.java
@@ -0,0 +1,30 @@
+package com.example.Veco.global.aws.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum S3ErrorCode {
+
+    FILE_SIZE_OVER(HttpStatus.BAD_REQUEST,
+            "S3400_0",
+            "파일 크기가 제한 크기를 초과하였습니다."),
+    NOT_FOUND_FILE(HttpStatus.BAD_REQUEST,
+            "S3400_1",
+            "이미지 파일이 존재하지 않습니다."),
+    NOT_IMAGE_FILE(HttpStatus.BAD_REQUEST,
+            "S3400_2",
+            "이미지 파일은 jpg, jpeg, png 파일만 가능합니다."),
+    IO_EXCEPTION(HttpStatus.BAD_REQUEST,
+            "S3400_3",
+            "파일 업로드 중 오류가 발생하였습니다."),
+    S3_EXCEPTION(HttpStatus.BAD_REQUEST,
+            "S3400_4",
+            "S3에 파일 업로드 실패하였습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
