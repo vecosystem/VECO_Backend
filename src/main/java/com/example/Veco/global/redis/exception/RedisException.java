888d3b44558468e5f89283e72746f51d1d1a5595
rlawngjs0313 <juheon0313@naver.com>
2025-07-13 15:17:20 +0900

    ♻️ refactor: Redis 예외 코드, Exception 공통 에러 상속

--- src/main/java/com/example/Veco/global/redis/exception/RedisException.java
+++ src/main/java/com/example/Veco/global/redis/exception/RedisException.java
@@ -1,9 +1,10 @@
 package com.example.Veco.global.redis.exception;
 
+import com.example.Veco.global.apiPayload.code.BaseErrorStatus;
 import com.example.Veco.global.apiPayload.exception.VecoException;
 
 public class RedisException extends VecoException {
-    public RedisException(String code) {
+    public RedisException(BaseErrorStatus code) {
         super(code);
     }
 }
--- src/main/java/com/example/Veco/global/redis/exception/code/RedisErrorCode.java
+++ src/main/java/com/example/Veco/global/redis/exception/code/RedisErrorCode.java
@@ -1,12 +1,14 @@
 package com.example.Veco.global.redis.exception.code;
 
+import com.example.Veco.global.apiPayload.ErrorReasonDTO;
+import com.example.Veco.global.apiPayload.code.BaseErrorStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public enum RedisErrorCode{
+public enum RedisErrorCode implements BaseErrorStatus {
 
     LOCK_TIMEOUT(HttpStatus.REQUEST_TIMEOUT,
             "REDIS408_0",
@@ -16,4 +18,14 @@ public enum RedisErrorCode{
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return ErrorReasonDTO.builder()
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
 }
