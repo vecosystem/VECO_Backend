81b59982dd6e78ab93b33df7e9ee9ebf00391412
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-07-20 13:35:27 +0900

    🛠️ fix: 알림로직 변경

--- src/main/java/com/example/Veco/domain/notification/service/NotificationCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.example.Veco.domain.notification.service;
-
-import com.example.Veco.global.event.NotificationSourceCreatedEvent;
-
-public interface NotificationCommandService {
-    void createNotificationAndMemberNotifications(NotificationSourceCreatedEvent event);
-}
