7c6e8d404b46dbaea28ce162c73cc8d7d61d82a9
박승범 <seoungbeom@bagseungbeom-ui-MacBookAir.local>
2025-07-21 17:47:22 +0900

    Feat:#1 외부 이슈 CRUD API 구현

--- src/main/java/com/example/Veco/domain/goal/entity/repository/GoalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Veco.domain.goal.entity.repository;
-
-import com.example.Veco.domain.goal.entity.Goal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoalRepository extends JpaRepository<Goal, Long> {
-}
--- src/main/java/com/example/Veco/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/Veco/domain/member/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import com.example.Veco.domain.member.enums.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
 
     Optional<Member> findBySocialUid(String socialUid);
+    List<Member> findAllByIdIn(List<Long> ids);
 }
