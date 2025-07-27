cefa9345e82497fd041541cf70f4e30731c5fb65
rlawngjs0313 <juheon0313@naver.com>
2025-07-27 21:02:40 +0900

    ✨ feat: 연동에 필요한 Link, ExternalService 엔티티 추가

---
+++ src/main/java/com/example/Veco/domain/mapping/entity/Link.java
@@ -0,0 +1,33 @@
+package com.example.Veco.domain.mapping.entity;
+
+import com.example.Veco.domain.external.entity.ExternalService;
+import com.example.Veco.domain.workspace.entity.WorkSpace;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "link")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "linked_at")
+    private LocalDateTime linkedAt;
+
+    // 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id")
+    private WorkSpace workspace;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "external_service_id")
+    private ExternalService externalService;
+}
