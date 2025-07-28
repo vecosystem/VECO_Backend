4f82a58dbadf8ba53b2e73d77542dfcddd0c300a
rlawngjs0313 <juheon0313@naver.com>
2025-07-29 05:18:36 +0900

    ♻️ refactor: Swagger 설명 추가

--- src/main/java/com/example/Veco/domain/slack/controller/SlackController.java
+++ src/main/java/com/example/Veco/domain/slack/controller/SlackController.java
@@ -4,32 +4,40 @@ import com.example.Veco.domain.slack.dto.SlackResDTO;
 import com.example.Veco.domain.slack.exception.code.SlackSuccessCode;
 import com.example.Veco.domain.slack.service.SlackCommandService;
 import com.example.Veco.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/slack")
+@Tag(name = "Slack 연동 API")
 public class SlackController {
 
     // 서비스
     private final SlackCommandService slackCommandService;
 
     // Slack 연동: 연동 시작 지점
+    @Operation(
+            summary = "Slack 연동 API By 김주헌",
+            description = "워크스페이스와 Slack을 연동합니다." +
+                    " 헤더에 JWT Access Token을 실어 보내시면 됩니다." +
+                    "요청이 오면 Slack OAuth 화면으로 리다이렉트되는 방식입니다."
+    )
     @GetMapping("/connect")
     public RedirectView slackConnect(
-//            @RequestHeader("Authorization") @Parameter(hidden = true)
-//            String token
+            @RequestHeader("Authorization") @Parameter(hidden = true)
+            String token
     ){
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMTMyMjkzNTA3ODM4Nzc1Mzc0NjIiLCJyb2xlIjoiIiwiaWF0IjoxNzUzNzMwODA0LCJleHAiOjE3NTM3MzQ0MDR9.ji6y-1TT_ctWTXMKf5uFup0n7pnTB_7lPs3sPapTMypvYMOuK9Bt-BcnUJKCPIAiZ-otRGLa5QgCSxs9-pa6Ig";
         return slackCommandService.redirectSlackOAuth(token);
     }
 
     // Slack Callback: 비즈니스 로직 시작 지점
+    @Hidden
     @GetMapping("/callback")
     public ApiResponse<SlackResDTO.InstallApp> installApp(
             @RequestParam String code,
