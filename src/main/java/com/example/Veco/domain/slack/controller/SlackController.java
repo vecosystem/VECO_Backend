82d4b44623fbcc7407b133c38ddfecf0262510c3
rlawngjs0313 <juheon0313@naver.com>
2025-07-29 05:08:34 +0900

    ♻️ refactor: Slack 연동 로직 변경 (/slack/connect 호출하는 방식)

--- src/main/java/com/example/Veco/domain/slack/controller/SlackController.java
+++ src/main/java/com/example/Veco/domain/slack/controller/SlackController.java
@@ -1,6 +1,7 @@
 package com.example.Veco.domain.slack.controller;
 
 import com.example.Veco.domain.slack.dto.SlackResDTO;
+import com.example.Veco.domain.slack.exception.code.SlackSuccessCode;
 import com.example.Veco.domain.slack.service.SlackCommandService;
 import com.example.Veco.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +19,22 @@ public class SlackController {
     // 서비스
     private final SlackCommandService slackCommandService;
 
+    // Slack 연동: 연동 시작 지점
+    @GetMapping("/connect")
+    public RedirectView slackConnect(
+//            @RequestHeader("Authorization") @Parameter(hidden = true)
+//            String token
+    ){
+        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMTMyMjkzNTA3ODM4Nzc1Mzc0NjIiLCJyb2xlIjoiIiwiaWF0IjoxNzUzNzMwODA0LCJleHAiOjE3NTM3MzQ0MDR9.ji6y-1TT_ctWTXMKf5uFup0n7pnTB_7lPs3sPapTMypvYMOuK9Bt-BcnUJKCPIAiZ-otRGLa5QgCSxs9-pa6Ig";
+        return slackCommandService.redirectSlackOAuth(token);
+    }
+
     // Slack Callback: 비즈니스 로직 시작 지점
     @GetMapping("/callback")
     public ApiResponse<SlackResDTO.InstallApp> installApp(
             @RequestParam String code,
             @RequestParam String state
     ){
-        return ApiResponse.onSuccess(slackCommandService.installApp(code, state));
+        return ApiResponse.onSuccess(SlackSuccessCode.CONNECTING, slackCommandService.installApp(code, state));
     }
 }
