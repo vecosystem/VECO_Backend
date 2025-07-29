4a066e74210c2684c69304459d55fae6ae99bb4c
rlawngjs0313 <juheon0313@naver.com>
2025-07-29 15:57:43 +0900

    ♻️ refactor: 담당자 targetId + category + 실제 객체 매핑 방식으로 리팩토링

--- src/main/java/com/example/Veco/domain/assignee/converter/AssigneeConverter.java
+++ src/main/java/com/example/Veco/domain/assignee/converter/AssigneeConverter.java
@@ -1,6 +1,7 @@
 package com.example.Veco.domain.assignee.converter;
 
 import com.example.Veco.domain.assignee.entity.Assignee;
+import com.example.Veco.domain.goal.entity.Goal;
 import com.example.Veco.domain.mapping.entity.MemberTeam;
 import com.example.Veco.global.enums.Category;
 
@@ -9,13 +10,14 @@ public class AssigneeConverter {
     // MemberTeam, id -> Assignee
     public static Assignee toAssignee(
             MemberTeam memberTeam,
-            Long targetId,
-            Category type
+            Category type,
+            Goal goal
     ){
         return Assignee.builder()
-                .targetId(targetId)
+                .targetId(goal.getId())
                 .type(type)
                 .memberTeam(memberTeam)
+                .goal(goal)
                 .build();
     }
 }
