938c318da4c685694985fd5dce5c1550e3ff0565
rlawngjs0313 <juheon0313@naver.com>
2025-08-03 23:06:42 +0900

    🔨 test: 쿠키, 서브 도메인으로 명시화

--- src/main/java/com/example/Veco/global/auth/jwt/util/JwtUtil.java
+++ src/main/java/com/example/Veco/global/auth/jwt/util/JwtUtil.java
@@ -220,7 +220,7 @@ public class JwtUtil {
         int maxAge = exp > 0 ? Math.toIntExact(exp / 1000) : 0;
 
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
-                .domain("vecoservice.shop")
+                .domain(".vecoservice.shop")
                 .httpOnly(true)
                 .secure(true)
                 .sameSite("None")
@@ -234,7 +234,7 @@ public class JwtUtil {
     public ResponseCookie expireRefreshTokenCookie() {
         // 만료된 refresh 토큰 쿠키 생성
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", "")
-                .domain("vecoservice.shop")
+                .domain(".vecoservice.shop")
                 .httpOnly(true)
                 .secure(true)
                 .sameSite("None")
