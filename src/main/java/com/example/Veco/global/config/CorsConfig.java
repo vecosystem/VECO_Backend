36716cdb0f154f4680ece9d044c971919094e295
rlawngjs0313 <juheon0313@naver.com>
2025-08-07 16:44:55 +0900

    🔨 test: flow 헤더 CORS 허용

--- src/main/java/com/example/Veco/global/config/CorsConfig.java
+++ src/main/java/com/example/Veco/global/config/CorsConfig.java
@@ -20,6 +20,7 @@ public class CorsConfig {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 모든 메소드 허용
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
+        configuration.addExposedHeader("Flow");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 적용
