69808660aeb3b2a8acc7de2be1bda6e253979b09
sobinMoon <sobin021206@naver.com>
2025-08-11 16:49:23 +0900

    🔨 fix: '매니저 없음' 필터에 대한 이슈 조회 오류 수정

--- src/main/java/com/example/Veco/domain/issue/controller/IssueController.java
+++ src/main/java/com/example/Veco/domain/issue/controller/IssueController.java
@@ -2,6 +2,9 @@ package com.example.Veco.domain.issue.controller;
 
 import com.example.Veco.domain.issue.dto.IssueReqDTO;
 import com.example.Veco.domain.issue.dto.IssueResponseDTO;
+import com.example.Veco.domain.issue.dto.IssueResponseDTO.Pageable;
+import com.example.Veco.domain.issue.dto.IssueResponseDTO.FilteringIssue;
+import com.example.Veco.domain.issue.dto.IssueResponseDTO.IssueWithManagers;
 import com.example.Veco.domain.issue.exception.code.IssueErrorCode;
 import com.example.Veco.domain.issue.exception.code.IssueSuccessCode;
 import com.example.Veco.domain.issue.service.IssueQueryService;
@@ -96,8 +99,9 @@ public class IssueController {
                     "커서 기반 페이지네이션, 최신 순으로 정렬합니다."
     )
     @GetMapping("/teams/{teamId}/issues")
-    public ApiResponse<IssueResponseDTO.Pageable<IssueResponseDTO.FilteringIssue<IssueResponseDTO.IssueWithManagers>>> getTeamIssues (
-            @PathVariable Long teamId,
+    public ApiResponse<Pageable<FilteringIssue<IssueWithManagers>>> getTeamIssues (
+            @PathVariable
+            Long teamId,
             @RequestParam(defaultValue = "-1")
             String cursor,
             @RequestParam(defaultValue = "1") @Min(value = 1, message = "불러올 데이터 수는 1 이상이어야 합니다.")
@@ -105,7 +109,7 @@ public class IssueController {
             @RequestParam(required = false, defaultValue = "state")
             String query
     ) {
-        IssueResponseDTO.Pageable<IssueResponseDTO.FilteringIssue<IssueResponseDTO.IssueWithManagers>> result = issueQueryService.getIssuesByTeamId(teamId, cursor, size, query);
+        Pageable<FilteringIssue<IssueWithManagers>> result = issueQueryService.getIssuesByTeamId(teamId, cursor, size, query);
         if (result != null) {
             return ApiResponse.onSuccess(IssueSuccessCode.OK, result);
         } else {
