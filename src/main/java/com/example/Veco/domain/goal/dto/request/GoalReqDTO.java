bf27f48deeb1f10383cd02ed368a08a219ce64d7
rlawngjs0313 <juheon0313@naver.com>
2025-08-11 21:11:18 +0900

    ✨ feat: 어노테이션 제약 조건 추가

--- src/main/java/com/example/Veco/domain/goal/dto/request/GoalReqDTO.java
+++ src/main/java/com/example/Veco/domain/goal/dto/request/GoalReqDTO.java
@@ -2,6 +2,8 @@ package com.example.Veco.domain.goal.dto.request;
 
 import com.example.Veco.global.enums.Priority;
 import com.example.Veco.global.enums.State;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,6 +12,8 @@ public class GoalReqDTO {
 
     // 목표 작성
     public record CreateGoal (
+            @Size(max = 20, message = "최대 20자까지 작성할 수 있습니다.")
+            @NotBlank(message = "제목은 반드시 작성해야 합니다.")
             String title,
             String content,
             State state,
