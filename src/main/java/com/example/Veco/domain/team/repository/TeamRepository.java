74757992c51057b369429af91b9c509b6fc0fc08
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-08-13 13:02:33 +0900

    fix: 기본팀은 항상 첫 순서 & 팀 생성시 order는 maxOrder+1

--- src/main/java/com/example/Veco/domain/team/repository/TeamRepository.java
+++ src/main/java/com/example/Veco/domain/team/repository/TeamRepository.java
@@ -5,6 +5,8 @@ import com.example.Veco.domain.workspace.entity.WorkSpace;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -20,4 +22,10 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByName(String name);
 
     boolean existsByNameAndWorkSpace(String name, WorkSpace workSpace);
+
+    @Query("select min(t.id) from Team t where t.workSpace = :workspace")
+    Long findMinTeamIdByWorkSpace(@Param("workspace") WorkSpace workspace);
+
+    @Query("select max(t.order) from Team t where t.workSpace = :workspace")
+    Optional<Integer> findMaxOrderByWorkSpace(@Param("workspace") WorkSpace workSpace);
 }
